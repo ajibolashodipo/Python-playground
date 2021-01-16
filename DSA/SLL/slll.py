74153f3d90432addafd482b12ccd30bc67641dc2
ajibolashodipo <shodipoajibola@gmail.com>
2021-01-16 23:53:48 +0000

    added sll partial implementation code. enjoying the ride

--- DSA/SLL/slll.py
+++ DSA/SLL/slll.py
@@ -1,35 +1,50 @@
-#  class Node:
-#      def __init__(self, data):
-#          self.data= data
-#          self.nextNode= None
-#
-#
-#
-# class LinkedList:
-#     def __init__(self):
-#         self.head= None
-#         self.tail= None
-#         self.numOfNodes= 0
-#
-#     def insert_start(self, data):
-#         self.numOfNodes += 1
-#         new_node= Node(data)
-#         # check if head exists
-#         if not self.head:
-#             self.head= new_node
-#             self.tail= self.head
-#
-#         else:
-#             old_head= self.head
-#             new_node.nextNode= old_head
-#             self.head= new_node
-
 class Node:
-    def __init__(self, value=None):
-        self.value= value
-        self.next= None
+    def __init__(self, val):
+        self.data = val
+        self.next = None
 
-class SLinkedList:
+
+class SinglyLinkedList:
     def __init__(self):
-        self.head= None
-        self.tail= None
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def push(self, val):
+        newNode = Node(val)
+        self.length += 1
+        if self.head is None:
+            self.head = newNode
+            self.tail = self.head
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def printList(self):
+        current = self.head
+        container = []
+        while current:
+            container.append(current.data)
+            current = current.next
+        return container
+
+    def printList_Recursive(self):
+        current = self.head
+        container = []
+
+        def helperFunction(curr):
+            if curr is not None:
+                container.append(curr.data)
+                helperFunction(curr.next)
+
+        helperFunction(current)
+        return container
+
+
+aj1 = SinglyLinkedList()
+aj1.push("ajibolaaa")
+aj1.push("ajibolaaa")
+aj1.push("111")
+aj1.push("222")
+print(aj1.printList())
+print(aj1.printList_Recursive())
