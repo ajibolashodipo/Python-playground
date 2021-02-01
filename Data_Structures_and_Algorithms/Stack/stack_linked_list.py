ed02653b33a22698df066a125dfb16560b70ebc5
ajibolashodipo <shodipoajibola@gmail.com>
2021-02-01 04:55:22 +0000

    did the Lord's work

--- Data_Structures_and_Algorithms/Stack/stack_linked_list.py
+++ Data_Structures_and_Algorithms/Stack/stack_linked_list.py
@@ -7,20 +7,29 @@ class Node:
 class Stack:
     def __init__(self):
         self.top = None
-        self.tail = None
         self.length = 0
 
     def push(self, val):
         newNode = Node(val)
+
+        # check if stack is full. essentially just check if newNode is null
+        if not newNode:
+            print("Stack overflow")
+            return
+
         self.length += 1
         if self.top is None:
             self.top = newNode
-            self.tail = self.top
         else:
             newNode.next = self.top
             self.top = newNode
 
     def pop(self):
+        # check for empty stack
+        if self.isEmpty():
+            print("Stack is empty")
+            return -1
+        # do the damn work
         old_head = self.top
         self.top = self.top.next
         return old_head.data
@@ -28,7 +37,6 @@ class Stack:
     def isEmpty(self):
         if self.top: return False
         return True
-
 
     def printList(self):
         current = self.top
@@ -40,12 +48,12 @@ class Stack:
         return container
 
 
-s= Stack()
-s.push(3)
-s.push(23)
-s.push(13)
-s.push(43)
-s.push(53)
+s = Stack()
+# s.push(3)
+# s.push(23)
+# s.push(13)
+# s.push(43)
+# s.push(53)
 s.pop()
 print(s.isEmpty())
 s.printList()
