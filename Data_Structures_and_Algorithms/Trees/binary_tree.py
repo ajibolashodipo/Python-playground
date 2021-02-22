8f30f3eeae75b7fd0921826f9948bb960f41013d
ajibolashodipo <shodipoajibola@gmail.com>
2021-02-22 04:51:07 +0000

    added recursive dfs for binary tree

--- Data_Structures_and_Algorithms/Trees/binary_tree.py
+++ Data_Structures_and_Algorithms/Trees/binary_tree.py
@@ -10,6 +10,35 @@ class BinaryTree:
         self.root = Node(root)
 
     def preorder_print(self):
+        curr = self.root
+        print(curr.left.value)
+        arr = []
+
+        def preorder_helper(curr):
+            if curr:
+                arr.append(curr.value)
+                preorder_helper(curr.left)
+                preorder_helper(curr.right)
+
+        preorder_helper(curr)
+        print(arr)
+        return arr
+
+    def inorder_print(self):
+        curr = self.root
+        arr = []
+
+        def inorder_helper(curr):
+            if curr:
+                inorder_helper(curr.left)
+                arr.append(curr.value)
+                inorder_helper(curr.right)
+
+        inorder_helper(curr)
+        print(arr)
+        return
+
+
 
 
 tree = BinaryTree(6)
@@ -19,3 +48,6 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(7)
 tree.root.right.left = Node(8)
 tree.root.right.right = Node(9)
+
+tree.preorder_print()
+tree.inorder_print()
