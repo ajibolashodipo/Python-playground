d454f7dacf7e145cda1b610e79948d5622f1ca5b
ajibolashodipo <shodipoajibola@gmail.com>
2021-05-26 23:59:04 +0100

    added howSum

--- Algorithms Deep Dive/DP/canSum.py
+++ Algorithms Deep Dive/DP/canSum.py
@@ -1,6 +1,5 @@
-
 def canSum(targetSum, numbers, memo={}):
-    print(memo)
+    # print(memo)
     if targetSum in memo:
         return memo[targetSum]
     if targetSum == 0:
@@ -27,8 +26,10 @@ def canSum(targetSum, numbers, memo={}):
 # works individually but dictionary doesn't clear between function calls.
 # so it doesnt return an accurate result if it all runs at once. take note
 
-print(canSum(7, [2, 3]))
-print(canSum(7, [5, 3, 4, 7]))
-print(canSum(7, [2, 4]))
-# print(canSum(8, [7,2, 3, 5]))
-# print(canSum(300, [7, 14]))  # very slow
+# FIX: Pass new memo dict in each function call. Makes sense
+
+print(canSum(7, [2, 3], memo={}))
+print(canSum(7, [5, 3, 4, 7], memo={}))
+print(canSum(7, [2, 4], memo={}))
+print(canSum(8, [7,2, 3, 5],memo={}))
+print(canSum(300, [7, 14],memo={}))  # very slow
---
+++ Algorithms Deep Dive/DP/howSum.py
@@ -0,0 +1,31 @@
+def howSum(targetSum, numbers, memo={}):
+    if targetSum in memo:
+        return memo[targetSum]
+    if targetSum == 0:
+        return []
+
+    if targetSum < 0:
+        return None
+
+    for num in numbers:
+        remainder = targetSum - num
+
+        # recursive call
+        remainderResult = howSum(remainder, numbers, memo)
+
+        if remainderResult is not None:
+            # equivalent of spread operator in js
+            memo[targetSum] = [*remainderResult, num]
+            return memo[targetSum]
+
+    # if for loop returns none,
+    memo[targetSum] = None
+    return None
+
+
+# print(howSum(7, [2, 3]))
+# print(howSum(7, [5, 3, 4, 7]))
+# print(howSum(7, [2, 4]))
+# print(howSum(7, [2, 4], memo={}))
+# print(howSum(8, [7,2, 3, 5],memo={}))
+print(howSum(300, [7, 14],memo={}))  # very slow
