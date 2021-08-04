8f0875cec1195c64f73def98ceb5fd88e152b4ee
ajibolashodipo <shodipoajibola@gmail.com>
2021-08-04 21:36:10 +0100

    added tabulation DP algos

---
+++ Algorithms Deep Dive/DP/tabulation/canSum_tabulation.py
@@ -0,0 +1,21 @@
+def canSum(targetSum, numbers):
+    # visualize solution as a table
+
+    # define a table size based on the inputs
+    table = [False] * (targetSum + 1)  # +1 cos of zero-based index
+
+    # seed trivial answers into the table
+    table[0] = True
+
+    # iterate through the table based on current position
+    for i in range(len(table)):
+        if table[i] is True:
+            for num in numbers:
+                if i + num <= targetSum:
+                    table[i + num] = True
+
+    print(table[targetSum])
+    return table[targetSum]
+
+
+canSum(300, [7, 14])
